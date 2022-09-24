8e79d39ec87c4ad7bcfa1119a64e4606cb7006cd
Greta <Greta1096@outlook.com>
2022-09-24 13:38:20 +0800

    创建 Layout 组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,48 @@
-import Nav from 'components/Nav';
+import Layout from 'components/Layout';
 import React from 'react';
-
 import {
   HashRouter as Router,
   Routes,
   Route,
   Navigate
 } from 'react-router-dom';
-import styled from 'styled-components';
-
-
-const Tags = () => (
-  <div><h2>标签页</h2></div>
-)
-const Money = () => (
-  <div><h2>记账页</h2></div>
-)
-
-const Statistics = () => (
-  <div><h2>统计页</h2></div>
-)
-const NoMatch = () => (
-  <div>
-    <h3>
-      路径写错啦
-    </h3>
-  </div>
-);
-const Wrapper = styled.div`
-/* border:1px solid red; */
-min-height: 100vh;
-display: flex;
-flex-direction: column;
-`
-const Main = styled.div`
-/* border: 1px solid green ; */
-flex-grow: 1;
-overflow: auto;
-`
 
 
 function App() {
   return (
     <Router>
-      <Wrapper>
-        <Main>
-          <Routes>
-            <Route path="/" element={<Navigate to="/money" />} />
-            <Route path="/tags" element={<Tags />} />
-            <Route path="/money" element={<Money />} />
-            <Route path="/statistics" element={<Statistics />} />
-            <Route path="*" element={<NoMatch />} />
-          </Routes>
-        </Main>
-        <Nav />
-      </Wrapper>
+      <Routes>
+        <Route path="/" element={<Navigate to="/money" />} />
+        <Route path="/tags" element={<Tags />} />
+        <Route path="/money" element={<Money />} />
+        <Route path="/statistics" element={<Statistics />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
     </Router>
   );
 }
 
+const Tags = () => (
+  <Layout>
+    <h2>标签页</h2>
+  </Layout>
+)
+const Money = () => (
+  <Layout>
+    <h2>记账页</h2>
+  </Layout>
+
+)
+
+const Statistics = () => (
+  <Layout>
+    <h2>统计页</h2>
+  </Layout>
+)
+const NoMatch = () => (
+  <h3>
+    路径写错啦
+  </h3>
+);
 
 export default App;
