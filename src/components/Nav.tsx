50e554decb7d198d4cf4d9234bdaba5b743a1737
Greta <Greta1096@outlook.com>
2022-09-24 15:29:46 +0800

    修复 Nav 点击区域过小的问题

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -13,7 +13,8 @@ line-height: 24px;
   > li{
     width:33.33333%;
     text-align: center;
-    display: flex;
+    > a {
+      display: flex;
     padding: 5px 0 3px 0;
     flex-direction: column;
     align-items: center;
@@ -21,6 +22,7 @@ line-height: 24px;
       width: 24px;
       height: 24px;
     }
+    } 
   }
 }
 `;
@@ -29,16 +31,24 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-          <Icon name="tag" />
-          <Link to="/tags">标签</Link>
+          <Link to="/tags">
+            <Icon name="tag" />
+            标签
+          </Link>
         </li>
         <li>
-          <Icon name="money" />
-          <Link to="/money">记账</Link>
+
+          <Link to="/money">
+            <Icon name="money" />
+            记账
+          </Link>
         </li>
         <li>
-          <Icon name="chart" />
-          <Link to="/statistics">统计</Link>
+
+          <Link to="/statistics">
+            <Icon name="chart" />
+            统计
+          </Link>
         </li>
       </ul>
     </NavWrapper>
