ac27dce08bb9070a4b192afa218902f6aff0b03d
Greta <Greta1096@outlook.com>
2022-10-01 17:12:00 +0800

    NumberPadSection 功能 wip（work in process） Category 添加 key

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,4 +1,3 @@
-import { Icon } from "components/Icon";
 import { Layout } from "components/Layout";
 import styled from "styled-components";
 import { NoteSection } from "./Money/NoteSection";
@@ -16,32 +15,7 @@ function Money() {
     <MyLayout>
       <TagsSection />
       <NoteSection />
-      <NumberPadSection>
-        <div className="output">100</div>
-        <div className="pad clearfix">
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>
-            <Icon name="date" />
-          </button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>
-            <Icon name="clear" />
-          </button>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>
-            <Icon name="back" />
-          </button>
-          <button>.</button>
-          <button>0</button>
-          <button className="ok">ok</button>
-        </div>
-      </NumberPadSection>
+      <NumberPadSection />
     </MyLayout>
   )
 }
--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,6 +1,8 @@
+// import { Icon } from "components/Icon";
+import React, { useState } from "react";
 import styled from "styled-components";
 
-const NumberPadSection = styled.section`
+const Wrapper = styled.section`
 display: flex;
 flex-direction: column;
   > .output{
@@ -9,9 +11,7 @@ flex-direction: column;
     line-height: 72px;
     text-align: right;
     padding: 0 16px;
-    box-shadow: 
-    /* inset 0 -5px 5px -5px rgba(0,0,0,0.25), */
-                inset 0 5px 5px -5px rgba(0,0,0,0.25);
+    box-shadow:  inset 0 5px 5px -5px rgba(0,0,0,0.25);
   }
   > .pad{
     background:#f5f5f5 ;
@@ -31,4 +31,65 @@ flex-direction: column;
     }
   }
 `
+const NumberPadSection: React.FC = () => {
+  const [output, setOutput] = useState('0')
+  const onClickButtonWrapper = (e: React.MouseEvent) => {
+    const text = (e.target as HTMLButtonElement).textContent
+    if (text === null) { return }
+    switch (text) {
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+      case '.':
+        if (output === '0') {
+          setOutput(text)
+        } else {
+          setOutput(output + text)
+        }
+        break;
+      case '日期':
+        console.log('日期')
+        break
+      case '清空':
+        console.log('清空')
+        break
+      case '删除':
+        console.log('删除')
+        break
+      case 'ok':
+        console.log('ok')
+        break
+
+    }
+  }
+  return (
+    <Wrapper>
+      <div className="output">{output}</div>
+      <div className="pad" onClick={onClickButtonWrapper}>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>日期</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>清空</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>删除</button>
+        <button>.</button>
+        <button>0</button>
+        <button className="ok">ok</button>
+      </div>
+    </Wrapper>)
+}
+
 export { NumberPadSection }
