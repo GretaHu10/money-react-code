7dbe73399c927c246e6fa3912b839a3a93b40d31
Greta <Greta1096@outlook.com>
2022-10-04 00:07:57 +0800

    封装 findTag 到 useTags 里，拿到 id 获取对应的 tag

--- src/views/TagEdit.tsx
+++ src/views/TagEdit.tsx
@@ -1,7 +1,16 @@
-const TagEdit: React.FC = () => {
+import { useParams } from "react-router-dom"
+import { useTags } from "useTags"
+
+type Params = {
+    id: string
+}
+const TagEdit: React.FC = (props) => {
+    const { findTag } = useTags()
+    let { id } = useParams<Params>()
+    const tag = findTag(parseInt(id!))
     return (
         <div>
-            hi
+            {tag.name}
         </div>
     )
 }
