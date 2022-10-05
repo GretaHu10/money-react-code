20a72fcc19e1d39f7dd6981cdf69fdd3071149e3
Greta <Greta1096@outlook.com>
2022-10-05 15:21:03 +0800

    删除 tag

--- src/views/TagEdit.tsx
+++ src/views/TagEdit.tsx
@@ -15,22 +15,16 @@ const InputWrapper = styled.div`
     margin-top: 8px;
 `
 
-
-
 type Params = {
     id: string
 }
+
 const TagEdit: React.FC = (props) => {
-    const { findTag, updateTag } = useTags()
+    const { findTag, updateTag, deleteTag } = useTags()
     let { id: idstring } = useParams<Params>()
     const tag = findTag(parseInt(idstring!))
-    return (
-        <Layout>
-            <Topbar>
-                <Icon name="left" />
-                <div>编辑标签</div>
-                <Icon />
-            </Topbar>
+    const tagContent = (tag: { id: number; name: string }) => (
+        <div>
             <InputWrapper>
                 <Input label="标签名" type="text" placeholder="可以写标签名啦"
                     value={tag.name}
@@ -38,9 +32,19 @@ const TagEdit: React.FC = (props) => {
                         updateTag(tag.id, { name: e.target.value })
                     }} />
             </InputWrapper>
+        </div>
+    )
+    return (
+        <Layout>
+            <Topbar>
+                <Icon name="left" />
+                <div>编辑标签</div>
+                <Icon />
+            </Topbar>
+            {tag ? tagContent(tag) : <Center><Gap /><Gap />tag 不存在</Center>}
             <Center>
                 <Gap /><Gap /><Gap />
-                <Button>删除标签</Button>
+                <Button onClick={() => deleteTag(tag.id)}>删除标签</Button>
             </Center>
         </Layout>
     )
