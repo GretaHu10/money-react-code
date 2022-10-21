4b0d2a0dcf2946bf1aab9112b68db3b3a0d9d212
Greta <Greta1096@outlook.com>
2022-10-21 21:39:04 +0800

    按时间分组展示统计数据

--- src/views/Statistics.tsx
+++ src/views/Statistics.tsx
@@ -1,5 +1,5 @@
 import { Layout } from "components/Layout";
-import { useRecords } from "hooks/useRecords";
+import { RecordItem, useRecords } from "hooks/useRecords";
 import { useTags } from "hooks/useTags";
 import { useState } from "react";
 import { CategorySection } from "./Money/CategorySection";
@@ -23,30 +23,53 @@ const Statistics = () => {
     const [category, setCategory] = useState<'-' | '+'>('-')
     const { records } = useRecords()
     const { getName } = useTags()
+    const hash: { [K: string]: RecordItem[] } = {}
     const selectedRecords = records.filter(r => r.category === category)
+
+    selectedRecords.map(r => {
+        const key = day(r.createdAt).format('YYYY-MM-DD')
+        if (!(key in hash)) {
+            hash[key] = []
+        }
+        hash[key].push(r)
+    })
+    const array = Object.entries(hash).sort((a, b) => {
+        if (a[0] === b[0]) return 0
+        if (a[0] > b[0]) return -1
+        if (a[0] < b[0]) return 1
+        return 0
+    })
+
+    console.log(array)
+
     return (
         <Layout>
             <CategorySection
                 value={category}
                 onChange={value => setCategory(value)} />
-            <div>
-                {selectedRecords.map(r => {
-                    return <Item>
-                        <div className="tags">
-                            {r.tagIds.map(tagId => <span>{getName(tagId)}</span>)}
-                        </div>
-                        {r.note && <div className="note">
-                            {r.note}
-                        </div>}
-                        <div className="amount">
-                            ￥{r.amount}
-                        </div>
-                        <div className="date">
-                            {day(r.createdAt).format('YYYY年MM月DD日')}
-                        </div>
-                    </Item>
-                })}
-            </div>
+            {array.map(([date, records]) => <div>
+                <h3>
+                    {date}
+                </h3>
+                <div>
+                    {records.map(r => {
+                        return <Item>
+                            <div className="tags">
+                                {r.tagIds.map(tagId => <span key={tagId}>{getName(tagId)}</span>)}
+                            </div>
+                            {r.note && <div className="note">
+                                {r.note}
+                            </div>}
+                            <div className="amount">
+                                ￥{r.amount}
+                            </div>
+                            <div className="date">
+                                {day(r.createdAt).format('YYYY年MM月DD日')}
+                            </div>
+                        </Item>
+                    })}
+                </div>
+            </div>)}
         </Layout>
     )
 }
